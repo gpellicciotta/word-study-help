9e13818519521c60d2de90517103c2a2e2b84f56
Giovanni Pellicciotta <giovanni.pellicciotta@gmail.com>
2025-01-19 22:46:39 +0100

    Better initial page

--- word-help.mjs
+++ word-help.mjs
@@ -1,8 +1,11 @@
 window.addEventListener("load", fireDomReady, false);
 
 function fireDomReady() {
-  let wordBox = document.getElementById("word-box");
+  let wordBox = document.getElementById("word-box");  
   wordBox.addEventListener("keydown", searchWord);
+  wordBox.value = "parola";
+  wordBox.focus();
+  wordBox.select(); // Select all
 }
 
 let openTabs = { };
@@ -16,16 +19,17 @@ function createLink(href, text, target = null) {
     a.target = target;
     a.onclick = function(event) {
       event.preventDefault();
-      if (!openTabs[target]) {
-        let newWindow = window.open(href, target);
-        newWindow.document.title = target;
+      if (openTabs[target]) {
+        openTabs[target].close();      
+      }
+      let newWindow = window.open(href, target);
+      if (newWindow) {
         openTabs[target] = newWindow;
-        openTabs[target].focus(); 
-      }
-      else {
-        openTabs[target].location.href = href;
-        openTabs[target].focus();
-      }
+        newWindow.addEventListener('load', function() {
+          newWindow.document.title = windowTitle;
+        });
+        newWindow.focus();
+      } 
     };        
   }
   return a;
@@ -41,23 +45,30 @@ function searchWord(e) {
   const dl = "en";
   const destinationLanguage = "english";
   
+  let wordEl = document.getElementById("word");
+  wordEl.innerHTML = word;
+
   let googleTranslate = document.getElementById("google-translate");
   let dictionary = document.getElementById("dictionary");
   let pronunciation = document.getElementById("pronunciation");
   let uses = document.getElementById("uses");
-  // Mela --> ?sl=it&tl=en&text=mela&op=translate
-  let googleTranslateLink = "http://translate.google.com?op=translate&hl=" + dl + " &sl=" + sl + "&tl=" + dl + "&text=" + word;
+  let images = document.getElementById("images");
+
+  // https://translate.google.com/?op=translate&hl=en%20&sl=it&tl=en&text=prigo
+  let googleTranslateLink = "https://translate.google.com?op=translate&hl=" + dl + " &sl=" + sl + "&tl=" + dl + "&text=" + word;
   googleTranslate.innerHTML = "";
   googleTranslate.appendChild(createLink(googleTranslateLink, destinationLanguage + " translation of '" + word + "'", "Translation"));	
-  let dictionaryLink = "http://" + sl + ".thefreedictionary.com/" + word;
+  let dictionaryLink = "https://" + sl + ".thefreedictionary.com/" + word;
   dictionary.innerHTML = "";
   dictionary.appendChild(createLink(dictionaryLink, "dictionary", "Dictionary"));
-  let pronunciationLink = "http://www.forvo.com/word/" + word + "/#" + sl;
+  let pronunciationLink = "https://www.forvo.com/word/" + word + "/#" + sl;
   pronunciation.innerHTML = "";
   pronunciation.appendChild(createLink(pronunciationLink, "pronunciation", "Pronunciation"));
   let usesLink = "https://context.reverso.net/translation/" + sourceLanguage + "-" + destinationLanguage + "/" + word;	
   uses.innerHTML = "";
   uses.appendChild(createLink(usesLink, "uses", "Uses"));		
-
+  let imagesLink = "https://www.google.com/search?hl=" + sl + "&sclient=img&udm=2&q=" + word;
+  images.innerHTML = "";
+  images.appendChild(createLink(imagesLink, "images", "Images"));
   wordBox.value = "";
 }
